52b5742b728265eee3edd900d2c83a3820bf9b02
Kordell Parrish <parrishkordell@gmail.com>
2020-07-31 01:05:23 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -5,3 +5,12 @@ def countdown(n)
   end
   return "HAPPY NEW YEAR!"
 end
+
+def countdown_with_sleep
+  while n > 0
+    puts "#{n} SECONDS(S)!"
+    n -= 1 
+    sleep 1 
+  end
+  return "HAPPY NEW YEAR!"
+end
